f056c0b4f78b3da5969f11ecf006dea33940d3ce
arthur <arthur.rodrigue64@gmail.com>
2025-09-01 20:28:07 -0300

    teste1

--- JavaApplication284/src/ProjPoo2/ProjPoo2.java
+++ JavaApplication284/src/ProjPoo2/ProjPoo2.java
@@ -1,21 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ProjPoo2;
 
-/**
- *
- * @author aluno.saolucas
- */
+import  view.Frlogin;
+
+
 public class ProjPoo2 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        FrLogin telaLogin = new FrLogin(); 
+        
     }
     
 }
